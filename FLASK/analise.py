c10ee9d232b6187670cca723538ee48aa8ddb2a8
Luis Felipe <luisfelipearaujo503@gmail.com>
2024-10-21 23:37:42 -0300

    Salvando mudanças 21-10-2024

---
+++ FLASK/analise.py
@@ -0,0 +1,5 @@
+# codigo adaptado do codigo de Polyana
+
+
+
+
--- FLASK/server.py
+++ FLASK/server.py
@@ -1,1 +1,38 @@
-fsgsgsgsg
+import numpy as np
+from flask import Flask, render_template, jsonify, request, send_from_directory
+import requests
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+'''def index():
+  return render_template('home.html')'''
+
+# Rota que recupera arquivos da pasta "assets"
+@app.route('/assets/<path:path>')
+def send_assets(path):
+  return send_from_directory('assets', path)
+
+
+# ROTA PRINCIPAL que mostra a página "resultado.html"
+@app.route('/analise', methods=['POST'])
+def analisar(videoInput):
+  '''
+  Pega video de input, executa método e retorna resultado como requisicao HTTP
+  '''
+  pass
+
+  
+@app.route('/demo', methods=['POST'])
+def demo_func():
+  '''
+  Pega video de demonstracao do servidor, finge processamento e retorna resultados
+  '''
+  pass
+
+if __name__ == '__main__':
+  # para poder adicionar um sheduler de tasks de background,
+  # adicionar use_reloader=False
+    app.run(debug=True)
