67f16948c8b5e3668414254dfb8bbf301f0e519e
Luis Felipe <luisfelipearaujo503@gmail.com>
2025-02-12 11:20:27 -0300

    Refactor df_videos function to improve label processing and add calculaVelocidadeEspacoPercorrido for velocity calculations

--- FLASK/TESTEMODELO.py
+++ FLASK/TESTEMODELO.py
@@ -33,7 +33,7 @@ URL_SERVER = "http://127.0.0.1:5000/analise"
  """
 
 
-def pegar_lista_paths(PATH):
+def pegar_lista_paths(PATH: str):
     lista = []
     for root, dirs, lista_arq in os.walk(PATH):
         lista = [os.path.join(root, elem) for elem in lista_arq]
@@ -49,7 +49,6 @@ def df_videos(PATH_SAUD: str, PATH_PAC: str, PATH_CSV: str):
     LISTAS
     '''
     lista_saudavel = pegar_lista_paths(PATH_SAUD)
-
     lista_pac = pegar_lista_paths(PATH_PAC)
 
     # adiciona todos os pacientes a uma so lista
@@ -69,52 +68,92 @@ def df_videos(PATH_SAUD: str, PATH_PAC: str, PATH_CSV: str):
 
     assert len(lista_pessoas) == len(lista_csv)
 
-    tuplas = []
+    indices = []
     colunas = ["FRAME", "X_ESQ", "Y_ESQ", "X_DIR", "Y_DIR"]
+    for path_pessoa in lista_pessoas:
+        id = os.path.splitext(path_pessoa)[0]
+        indices.append(id)
+
+    df_labels = pd.DataFrame(index=indices, columns=[
+                             "ARRAY_VEL", "DIF", "OLHO_DOENTE"])
+    # povoa df que contem as labels
     for path_pessoa, path_csv in zip(lista_pessoas, lista_csv):
         id = os.path.splitext(path_pessoa)[0]
         df_csv = pd.read_csv(path_csv, index_col=0,
                              names=colunas, delimiter=",")
-        lista_frames = list(df_csv.index)
-        tuplas.extend(product([id], lista_frames))
+        pos_esq = df_csv.loc[:, "X_ESQ"].to_list()
+        pos_dir = df_csv.loc[:, "X_DIR"].to_list()
+        velE, velD = calculaVelocidadeEspacoPercorrido(pos_esq, pos_dir)
 
-    tuplas = tuple(tuplas)
-    indices = pd.MultiIndex.from_tuples(tuplas)
-    df_labels = pd.DataFrame(index=indices, columns=[
-                             "X_ESQ", "Y_ESQ", "X_DIR", "Y_DIR"])
-    # povoa df multiindex que contem as labels
-    for path_pessoa, path_csv in zip(lista_pessoas, lista_csv):
-        id = path_pessoa.split(".")[0]
-        df_csv = pd.read_csv(path_csv, index_col=0,
-                             names=colunas, delimiter=",")
-        for frame in df_csv.index:
-            df_labels.loc[(id, frame), ["X_ESQ", "Y_ESQ", "X_DIR", "Y_DIR"]
-                          ] = df_csv.loc[frame, ["X_ESQ", "Y_ESQ", "X_DIR", "Y_DIR"]]
+        percentDif = 1 - min(velE, velD) / max(velE, velD)
+        threshold = 0.1965  # 19.65%, ver artigo
+        olho_doente = ""
+
+        if velE < velD:
+            olho_doente = "Esquerdo"
+        else:
+            olho_doente = "Direito"
+        if percentDif < threshold:
+            olho_doente = None
+
+        df_labels.loc[id, ("ARRAY_VEL", "DIF", "OLHO_DOENTE")] = [
+            [velE, velD], percentDif, olho_doente]
+
     return df_labels, indices
 
 
-def pegar_dados_resp(resposta: requests.Response):
-    pass
+def calculaVelocidadeEspacoPercorrido(xEsquerdo: list, xDireito: list) -> tuple[float, float]:
+    assert type(xDireito) == type(xEsquerdo) == list
+    somaEsquerda = 0
+    somaDireita = 0
+    if sum(xEsquerdo) > sum(xDireito):
+        maxIndex = np.where(xEsquerdo == np.amax(xEsquerdo))
+        minIndex = np.where(xEsquerdo == np.amin(xEsquerdo))
+        mE = min(minIndex[0][0], maxIndex[0][0])
+        mxE = max(minIndex[0][0], maxIndex[0][0])
+        length = abs(mxE - mE)
+        for i in range(mE + 1, mxE + 1):
+            somaEsquerda += abs(xEsquerdo[i] - xEsquerdo[i - 1])
+            somaDireita += abs(xDireito[i] - xDireito[i - 1])
+        velE = somaEsquerda / length
+        velD = somaDireita / length
+    else:
+        maxIndex = np.where(xDireito == np.amax(xDireito))
+        minIndex = np.where(xDireito == np.amin(xDireito))
+        mE = min(minIndex[0][0], maxIndex[0][0])
+        mxE = max(minIndex[0][0], maxIndex[0][0])
+        length = abs(mxE - mE)
+        for i in range(mE + 1, mxE + 1):
+            somaEsquerda += abs(xEsquerdo[i] - xEsquerdo[i - 1])
+            somaDireita += abs(xDireito[i] - xDireito[i - 1])
+        velE = somaEsquerda / length
+        velD = somaDireita / length
+    return velE, velD
 
 
 def testar_api(lista_saudaveis, lista_pacientes, df_labels, df_res):
     lista_resp = []
     for pac in lista_saudaveis:
-
         # Open the video file in binary read mode
         with open(pac, 'rb') as video_file:
-            # Create a dictionary for the files parameter
+
             ext = os.path.splitext(pac)[1]
             mimetype = mimetypes.types_map[ext]
+            # Create a dictionary for the files parameter
             files = {'file': (f'video.{ext}', video_file, mimetype)}
-
             # Send the POST request with the video file
             response = requests.post(URL_SERVER, files=files)
-            video_pac = cv2.VideoCapture(pac)
-
-            lista_resp.append(response.text)
-            print(response.text)
+            parse_response(response)
+            lista_resp.append(response.json())
+            print(response.json())
             break
+
+
+def parse_response(resposta: requests.Response):
+    dict_resp = resposta.json()
+    str_res = dict_resp["string"]
+    velE, velD, difPercent, olho_doente = str_res.split(",")
+    return [velE, velD, difPercent, olho_doente]
 
 
 # MODE LOCAL == running outside of Google Colab"
@@ -123,8 +162,8 @@ if os.path.exists("/content"):
     MODO = "COLAB"
 else:
     MODO = "LOCAL"
-# --SETTING PATH_DATASET-- #
 
+# --SETTING PATH_DATASET-- #
 CWD = os.getcwd()
 PATH_DADOS = os.path.join(CWD, "DADOS")
 
@@ -140,15 +179,11 @@ if any(not os.path.exists(elem) for elem in [PATH_PACIENTES, PATH_SAUDAVEIS,  PA
     raise SystemError("dados para teste nao existem")
 
 #### LOOP PRINCIPAL ####
-df_labels, multi_index = df_videos(PATH_SAUDAVEIS, PATH_PACIENTES, PATH_CSV)
-df_res = pd.DataFrame(columns=["X_ESQ", "Y_ESQ", "X_DIR",
-                      "Y_DIR", "DIF_VEL", "DIAG", "TEMP_PROC"], index=multi_index)
+df_labels, index = df_videos(PATH_SAUDAVEIS, PATH_PACIENTES, PATH_CSV)
+df_res = pd.DataFrame(columns=df_labels.columns, index=index)
 
-
-# TODO: criar loop  pra fazer teste da API com a lista de videos que ja existe
+# TODO: criar loop pra fazer teste da API com a lista de videos que ja existe
 lista_saudavel = pegar_lista_paths(PATH_SAUDAVEIS)
 lista_pac = pegar_lista_paths(PATH_PACIENTES)
-
 testar_api(lista_saudavel, lista_pac, df_labels, df_res)
-
 # TODO: criar nova rota no Flask pra lidar com teste da API (precisa de mais dados)
