131dabe34421dab767691787d9f6253fa2955c92
Luis PC DESKTOP <luisfelipearaujo503@gmail.com>
2025-07-16 16:01:28 -0300

    Modificando compose com IP próprio da VM

--- vite-project/src/store/index.js
+++ vite-project/src/store/index.js
@@ -10,7 +10,7 @@ import { useCookies } from 'vue3-cookies';
 
 const store = createStore({
     state:{
-        urlBackend: "http://127.0.0.1:5000",
+        urlBackend: import.meta.env.VITE_API_URL,
         sharedData: null,
         logado: false,
         formDiag: null,
