c2d1b748de1d0d929136c4f9ca2cb756f7fad750
Luis PC DESKTOP <luisfelipearaujo503@gmail.com>
2025-08-25 17:41:09 -0300

    ajeitando versao do Celery

--- flask_backend/celery_worker/celery.py
+++ flask_backend/celery_worker/celery.py
@@ -2,11 +2,14 @@ from celery import Celery
 import os
 
 app = Celery(
-    "celery_worker",  # DEVE SER O NOME DA PASTA QUE ESTAO OS ARQUIVOS
+    "flask_backend",  # DEVE SER O NOME DA PASTA QUE ESTAO OS ARQUIVOS
     backend=os.environ["CELERY_RESULT_BACKEND"],
     broker=os.environ["CELERY_BROKER_URL"],
-    include=["celery_worker.tasks"],  # pasta.tasks
+    include=["flask_backend.celery_worker.tasks"],  # <-- corrigido
 )
+
+app.autodiscover_tasks(["flask_backend.celery_worker"])
+
 
 if __name__ == "__main__":
     app.start()
