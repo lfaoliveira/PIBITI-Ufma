9c51f63671257194b1244d425f3f5a5342725f6a
Luis PC DESKTOP <luisfelipearaujo503@gmail.com>
2025-08-29 16:36:10 -0300

    adicionando estrela

--- vite-project/src/components/icons/Estrela.vue
+++ vite-project/src/components/icons/Estrela.vue
@@ -1,21 +1,5 @@
 <template>
-    <div class="flex items-center justify-center">
-        <!-- Circle background -->
-        <div
-            class="flex items-center justify-center rounded-full bg-purple-900 w-12 h-12"
-        >
-            <!-- Star Icon -->
-            <span class="material-icons text-white text-3xl"> star </span>
-        </div>
-    </div>
+    <img src="../../assets/estrela_roxa.svg"></img>
 </template>
 
-<script>
-export default {
-    name: "Estrela",
-};
-</script>
-
-<style>
-/* MATERIAL ICONS SENDO IMPORTADO NO /src/style.css */
-</style>
+<script lang="ts" setup></script>
