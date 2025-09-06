d0f8e99ba5ce647dcc4367cdefe0995c86ce08f0
Luis PC DESKTOP <luisfelipearaujo503@gmail.com>
2025-09-06 11:10:10 -0300

    ajustando paths nos codigos de Polyana

--- flask_backend/__init__.py
+++ flask_backend/__init__.py
@@ -1,3 +1,12 @@
+import os
+
 COLLECTION_DIAGS = "Diagnosticos"
 COLLECTION_MEDICOS = "Medicos"
 PASTA_USUARIO_ANONIMO_GDRIVE = "ANONIMO"
+
+PACKAGE_WKDIR = os.path.join(
+    os.path.join(
+        os.getcwd(),
+        __package__,
+    )
+)
